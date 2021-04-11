f4798ad18b2d02bd941895dbcad9493bbbf5b53e
raichek23 <65887972+raichek23@users.noreply.github.com>
2021-04-12 00:56:07 +0900

    Update main.cpp
    - Add filestream

--- main.cpp
+++ main.cpp
@@ -1,42 +1,60 @@
 #include <string>
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-class Contact {
-private:
-    int _id;
-    string _name;
-    string _mail;
-    string _memo;
-public:
-    Contact(int id, string name, string mail, string memo) {
-        _id = id;
-        _name = name;
-        _mail = mail;
-        _memo = memo;
-    }
-    string getName() {
-        return _name;
-    }
-    void setName(string name) {
-        return 0;
-    }
-    string getMail() {
-        return _mail
-    }
-    string setMail(string mail) {
-        return _mail;
-    }
-    string getMemo() {
-        return _memo;
-    }
-    string setMemo(string memo) {
-        return _memo;
-    }
-    int checkAddData(Contact contact) {
-    }
-};
+// class Contact {
+// private:
+//     int _id;
+//     string _name;
+//     string _mail;
+//     string _memo;
+// public:
+//     Contact(int id, string name, string mail, string memo) {
+//         _id = id;
+//         _name = name;
+//         _mail = mail;
+//         _memo = memo;
+//     }
+//     string getName() {
+//         return _name;
+//     }
+//     void setName(string name) {
+//         return 0;
+//     }
+//     string getMail() {
+//         return _mail
+//     }
+//     string setMail(string mail) {
+//         return _mail;
+//     }
+//     string getMemo() {
+//         return _memo;
+//     }
+//     string setMemo(string memo) {
+//         return _memo;
+//     }
+//     int checkAddData(Contact contact) {
+//     }
+// };
+
+// 日時の文字列取得
+char getDatetimeStr() {
+    time_t t = time(nullptr);
+    const tm* localTime = localtime(&t);
+    stringstream s;
+    s << "/workspace/cp_Contacts/20" << localTime->tm_year - 100;
+    s << setw(2) << setfill('0') << localTime->tm_mon + 1;
+    s << setw(2) << setfill('0') << localTime->tm_mday;
+    s << setw(2) << setfill('0') << localTime->tm_hour;
+    s << setw(2) << setfill('0') << localTime->tm_min;
+    s << setw(2) << setfill('0') << localTime->tm_sec;
+    s << ".txt";
+    return s.get();
+}
 
 // メニュー表示
 int viewMenu() {
@@ -55,16 +73,19 @@ int viewMenu() {
     return input;
 }
 
-Contact contact;    // インスタンス
+// Contact contact;    // インスタンス
 
 // メイン処理
 int main() {
+    const char *fileName = getDatetimeStr();
+    ofstream ofs(fileName);
+
     while(1) {
         int input = 0;
         input = viewMenu();
         switch(input) {
             case 1:
-                cout >> "Please enter your name." >> endl;
+                cout << "Please enter your name." << endl;
                 break;
             case 2:
                 break;
@@ -75,6 +96,13 @@ int main() {
             case 5:
                 break;
             case 6:
+                if(!ofs) {
+                    cout << "Can't open the file." << endl;
+                    cin.get();
+                } else {
+                    ofs << "Hello, world!2" << endl;
+                    cin.get();
+                }
                 break;
             case 7:
                 break;
